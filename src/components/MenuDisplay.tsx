099b9272f5b5e5cb16df4fcf31de7cb7e600578c
nununuma-sabu <niigata.info.kt@gmail.com>
2026-02-28 06:17:37 +0900

    UI修正: アコーディオン開閉時に隣のカードの高さが連動して伸びないように items-start を追加

--- src/components/MenuDisplay.tsx
+++ src/components/MenuDisplay.tsx
@@ -168,7 +168,7 @@ export default function MenuDisplay({ menu, dailyTarget }: MenuDisplayProps) {
             )}
 
             {/* Meals for selected day */}
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 items-start">
                 {currentDay.meals && currentDay.meals.map((meal, index) => (
                     <MealCard key={`${activeDay}-${index}`} meal={meal} />
                 ))}
